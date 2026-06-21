58d9134dda16d9999eb36721cc0bb9faf1fdea5c
SamuelCagnani <samuelcagnani@gmail.com>
2026-06-21 19:04:56 -0300

    fix: force Docker build cache invalidation
    - Add ARG CACHE_BUST to frontend Dockerfile
    - rebuild.sh: docker builder prune -af before build
    - rebuild.sh: pass timestamp as --build-arg CACHE_BUST

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,5 @@
-docker compose down -v && docker compose up --build -d
+#!/bin/bash
+docker compose down -v
+docker builder prune -af
+docker compose build --no-cache --build-arg CACHE_BUST="$(date +%s)"
+docker compose up -d
